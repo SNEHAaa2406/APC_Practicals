0e1ee08fc20c92cd43739d1d2aa6e50672ee5dfd
Sneha mulik <snehamulik167@gmail.com>
2026-07-16 21:56:38 +0530

    Add files via upload

---
+++ practical1.py
@@ -0,0 +1,30 @@
+# #INT
+# a=int(input('enter 1st value '))
+# b=int(input('enter 2nd value'))
+# c=a+b
+# print("answer",c)
+
+
+#Float
+# x=float(input('enter 1st value'))
+# y=float(input('enter 2nd value'))
+# z=x+y
+# print("answer",z)
+
+
+#list
+# x = ["apple", "banana", "cherry","watermelon"]
+# print(x)
+
+#tuple
+# x = ("apple", "banana", "cherry","watermelon")
+# print(x)
+
+x=(input('enter 1st product'))
+y=(input("enter 2nd product"))
+z=(input("enter 3rd product"))
+print(x,y,z);
+   
+#set 
+# x = {"apple", "banana", "cherry","watermelon"}
+# print(x)
