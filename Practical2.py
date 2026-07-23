0e8d3ff35c4e0880c3127f271b9b885005999919
Sneha mulik <snehamulik167@gmail.com>
2026-07-23 10:27:36 +0530

    Add files via upload

---
+++ Practical2.py
@@ -0,0 +1,22 @@
+# Print all natural numbers upto n
+# n=int(input("Enter value of n :"))
+# for i in range (1,n+1):
+#     print(i)
+
+# print even numbers upto n 
+# n=int(input("enter the numbet:"))
+# for i in range(n+2):
+#     if(i%2==0):
+#         print(i)
+
+#Program to find odd numbers upto n
+# n=int(input("Enter value of n :"))
+# for i in range (n+1):
+#     if(i%2!=0):
+#         print(i)
+
+#Program that prints 1,2,4,8,10,32..
+# n=int(input("Enter value of n :"))
+# for i in range(n+1):
+#     print(2**i)
+#     i=i+1
