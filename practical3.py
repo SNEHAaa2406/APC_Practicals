6749e6ede121a0e2bd2b2a624614e1aa2b0871c8
Sneha mulik <snehamulik167@gmail.com>
2026-07-30 10:42:43 +0530

    Add files via upload

---
+++ practical3.py
@@ -0,0 +1,103 @@
+# per=int(input("Enter the percentage you got :"))
+# if (per>=90):
+#     print("Excellent performance")
+# elif(per>=80):
+#     print("Very good performance")
+# elif(per>=70):
+#     print("Good performance")
+# elif(per>=60):
+#     print("Average performance")
+# elif("per<60"):
+#     print("Need improvment")
+# else:
+#     print("Something wrong")
+# ----------------------------------------------------
+# 2.largest of 3
+# a=int(input("Enter value of 1st element :"))
+# b=int(input("Enter value of 2nd element :"))
+# c=int(input("Enter value of 3rd element :"))
+# if(a>b and a>c):
+#     print("Largest element is :",a)
+# elif(b>c and b>a):
+#     print("Largest element is :",b)
+# elif(c>a and c>b):
+#     print("Largest element is :",c)
+# elif(a==b and b==c):
+#     print("All are of equal strength")
+# -------------------------------------------------
+# 3.Smallest element 
+# a=int(input("Enter value of 1st element :"))
+# b=int(input("Enter value of 2nd element :"))
+# c=int(input("Enter value of 3rd element :"))
+# if(a<b and a<c):
+#     print("Smallest element is :",a)
+# elif(b<c and b<a):
+#     print("Smallest element is :",b)
+# elif(c<a and c<b):
+#     print("Smallest element is :",c)
+# elif(a==b and b==c):
+#     print("All are of equal strength")
+# ---------------------------------------
+# 4.Check if its even or odd
+# n=int(input("Enter the element:"))
+# if(n%2==0):
+#     print("This is a even number")
+# elif(n%2==1):
+#     print("This is a odd number")
+# ---------------------------------------
+# 5. Check year if it's leap or not
+# n=int(input("Enter the year :"))
+# if(n%4==0):
+#     print("Its a leap year")
+# else:
+#     print("It is not a leap year.")
+# -------------------------------------------
+# 6.Check if the drriver is insured or not
+
+# name=str(input("Enter your name :"))
+# gender=str(input("Enter your gender :"))
+# n=int(input(("Enter your age :")))
+# que=str(input("Are you married ?"))
+# if(que=="no" and n>30 and gender=="male"):
+#     print("ensured")
+# elif(que=="no"and n>25 and gender=="female"):
+#     print("Ensured")
+# else:
+#     print("unensured")
+# ---------------------------------------------
+# 7.check number if it's zero or not.
+# n=int(input("Enter the number:"))
+# if(n==0):
+#     print("The number is zero")
+# else:
+#     print("The numbers is non zero")
+# ---------------------------------------
+# 8. Largest of 2 
+# a=int(input("Enter value of a :"))
+# b=int(input("Enter value of b :"))
+# if(a>b):
+#     print("A is greater")
+# elif(b>a):
+#     print("B is greater")
+# elif(a==b):
+#     print("Both are same ")
+# else:
+#     print("Unavailable")
+# ------------------------------
+#9. check if its positive and negative
+# a=int(input("Enter value  :"))
+# if(a>=0):
+#     print("Positive number")
+# elif(a<0):
+#     print("Negative number")
+# else:
+#     print("something went wrong")
+# ------------------------------------
+# 10. constant or consonant
+# n=str(input("Enter a letter"))
+# letters="aeiouAEIOU"
+# if(n in letters):
+#     print("vowel")
+# else:
+#     print("Consonant")
+# ---------------------------
